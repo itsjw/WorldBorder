f494ab95de6109e63e6983c51eb22aa7e99fb261
Mystiflow <mystiflow@gmail.com>
2017-09-12 20:19:47 +0100

    Checkstyle changes

--- src/main/java/com/mystiflow/worldborder/impl/BasicWorldBorder.java
+++ src/main/java/com/mystiflow/worldborder/impl/BasicWorldBorder.java
@@ -1,11 +1,25 @@
 /*
+ * The MIT License (MIT)
+ *
  * Copyright (C) 2017 Mystiflow <mystiflow@gmail.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.mystiflow.worldborder.impl;
 
@@ -27,98 +41,101 @@ public class BasicWorldBorder implements WorldBorder {
     /**
      * Creates a new WorldBorder with a given center and distance.
      *
-     * @param centerX the x center
-     * @param centerZ the z center
+     * @param centerX  the x center
+     * @param centerZ  the z center
      * @param distance the distance in blocks
      */
-    public BasicWorldBorder(int centerX, int centerZ, int distance) {
+    public BasicWorldBorder(final int centerX, final int centerZ,
+                            final int distance) {
         this.centerX = centerX;
         this.centerZ = centerZ;
         this.distance = distance;
     }
 
     @Override
-    public int getMinX() {
-        return getCenterX() - distance;
+    public final int getMinX() {
+        return centerX - distance;
     }
 
     @Override
-    public int getMaxX() {
-        return getCenterX() + distance;
+    public final int getMaxX() {
+        return centerX + distance;
     }
 
     @Override
-    public int getMinZ() {
-        return getCenterZ() - distance;
+    public final int getMinZ() {
+        return centerZ - distance;
     }
 
     @Override
-    public int getMaxZ() {
-        return getCenterZ() + distance;
+    public final int getMaxZ() {
+        return centerZ + distance;
     }
 
     @Override
-    public int getCenterX() {
+    public final int getCenterX() {
         return centerX;
     }
 
     @Override
-    public void setCenterX(int centerX) {
+    public final void setCenterX(final int centerX) {
         this.centerX = centerX;
     }
 
     @Override
-    public int getCenterZ() {
+    public final int getCenterZ() {
         return centerZ;
     }
 
     @Override
-    public void setCenterZ(int centerZ) {
+    public final void setCenterZ(final int centerZ) {
         this.centerZ = centerZ;
     }
 
     @Override
-    public int getDistance() {
+    public final int getDistance() {
         return distance;
     }
 
     @Override
-    public void setDistance(int distance) {
+    public final void setDistance(final int distance) {
         this.distance = distance;
     }
 
     @Override
-    public boolean isInBounds(int x, int z) {
-        return x >= (centerX - distance) && x <= (centerX + distance)
-                && z >= (centerZ - distance) && z <= (centerZ + distance);
+    public final boolean isInBounds(final int x, final int z) {
+        return (x >= centerX - distance) && (x <= centerX + distance)
+                && (z >= centerZ - distance) && (z <= centerZ + distance);
     }
 
     @Override
-    public boolean isInBounds(Entity entity) {
+    public final boolean isInBounds(final Entity entity) {
         requireNonNull(entity, "Location is null");
         return this.isInBounds(entity.getBlockX(), entity.getBlockZ());
     }
 
     @Override
-    public boolean isInBounds(Location location) {
+    public final boolean isInBounds(final Location location) {
         requireNonNull(location, "Location is null");
         return this.isInBounds(location.getBlockX(), location.getBlockZ());
     }
 
     @Override
-    public boolean isInBounds(Block block) {
+    public final boolean isInBounds(final Block block) {
         requireNonNull(block, "Block is null");
         return this.isInBounds(block.getX(), block.getZ());
     }
 
     @Override
-    public double getKnockbackDistance() {
+    public final double getKnockbackDistance() {
         return knockbackDistance;
     }
 
     @Override
-    public void setKnockbackDistance(double knockbackDistance) throws IllegalArgumentException {
-        checkArgument(knockbackDistance >= 0, "Knockback distance must be a positive number");
+    public final void setKnockbackDistance(final double knockbackDistance)
+            throws IllegalArgumentException {
+        checkArgument(knockbackDistance >= 0,
+                "Knockback distance must be a positive number");
         this.knockbackDistance = knockbackDistance;
     }
 }
--- src/main/java/com/mystiflow/worldborder/impl/BasicWorldBorderHandler.java
+++ src/main/java/com/mystiflow/worldborder/impl/BasicWorldBorderHandler.java
@@ -1,17 +1,31 @@
 /*
+ * The MIT License (MIT)
+ *
  * Copyright (C) 2017 Mystiflow <mystiflow@gmail.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.mystiflow.worldborder.impl;
 
 import com.mystiflow.worldborder.WorldBorderPlugin;
-import com.mystiflow.worldborder.api.WorldBorderHandler;
 import com.mystiflow.worldborder.api.WorldBorder;
+import com.mystiflow.worldborder.api.WorldBorderHandler;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 
@@ -19,55 +33,68 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 
+import static java.lang.System.lineSeparator;
 import static java.util.Objects.requireNonNull;
 
 public class BasicWorldBorderHandler implements WorldBorderHandler {
+
+    private static final double DEFAULT_KNOCKBACK = 2.5D;
+    private static final int DEFAULT_DISTANCE = 2500;
 
     private final Map<String, WorldBorder> worldBorders = new HashMap<>();
 
     private final WorldBorderPlugin plugin;
 
-    public BasicWorldBorderHandler(WorldBorderPlugin plugin, ConfigurationSection section) {
+    public BasicWorldBorderHandler(final WorldBorderPlugin plugin,
+                                   final ConfigurationSection section) {
         this.plugin = plugin;
         this.setupConfiguration(section);
     }
 
-    private void setupConfiguration(ConfigurationSection section) {
+    private void setupConfiguration(final ConfigurationSection section) {
         if (section.isConfigurationSection("worlds")) {
-            final ConfigurationSection worldSection = section.getConfigurationSection("worlds");
-            for (String worldName : worldSection.getKeys(false)) {
-                final int centerX = worldSection.getInt(worldName + ".centerX", 0);
-                final int centerZ = worldSection.getInt(worldName + ".centerZ", 0);
-                final int distance = worldSection.getInt(worldName + ".distance", 10000);
+            final ConfigurationSection wSection = section
+                    .getConfigurationSection("worlds");
 
-                WorldBorder worldBorder = new BasicWorldBorder(centerX, centerZ, distance);
-                worldBorder.setKnockbackDistance(worldSection.getDouble(worldName + ".knockback-distance", 2.5));
+            for (String wName : wSection.getKeys(false)) {
+                final int centerX = wSection.getInt(wName + ".centerX", 0);
+                final int centerZ = wSection.getInt(wName + ".centerZ", 0);
+                final int distance = wSection.getInt(
+                        wName + ".distance", DEFAULT_DISTANCE);
+                final double knockback = wSection.getDouble(
+                        wName + ".knockback-distance", DEFAULT_KNOCKBACK);
 
-                this.worldBorders.put(worldName, worldBorder);
+                WorldBorder wb = new BasicWorldBorder(
+                        centerX, centerZ, distance);
+                wb.setKnockbackDistance(knockback);
+                this.worldBorders.put(wName, wb);
 
-                plugin.getLogger().log(Level.INFO, "Created world border for world '" + worldName + "' with distance " + distance);
-                plugin.getLogger().log(Level.INFO, "Knockback distance of '" + worldName + "' is " + worldBorder.getKnockbackDistance());
+                this.plugin.getLogger().log(Level.INFO, "Created border"
+                        + " for world '" + wName + "'!" + lineSeparator()
+                        + "Knockback=" + knockback + lineSeparator()
+                        + "Distance = " + distance
+                );
             }
         }
     }
 
     @Override
-    public WorldBorder getWorldBorder(World world) {
-        return this.getWorldBorder(requireNonNull(world, "world is null").getName());
+    public final WorldBorder getBorder(final World world) {
+        return this.getBorder(
+                requireNonNull(world, "world is null").getName());
     }
 
     @Override
-    public WorldBorder getWorldBorder(String worldName) {
-        requireNonNull(worldName, "worldName is null");
-
-        return worldBorders.get(worldName);
+    public final WorldBorder getBorder(final String worldName) {
+        return this.worldBorders.get(
+                requireNonNull(worldName, "worldName is null"));
     }
 
     @Override
-    public void setWorldBorder(String worldName, WorldBorder worldBorder) {
+    public final void setBorder(final String worldName,
+                                final WorldBorder worldBorder) {
         requireNonNull(worldName, "worldName is null");
         requireNonNull(worldBorder, "worldBorder is null");
-
         this.worldBorders.put(worldName, worldBorder);
     }
 }
